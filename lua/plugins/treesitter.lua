caf0a5e330195d10c43aa892dceb536082f6504c
ishrq <p.git@ishrq.com>
2025-12-16 23:55:23 +0600

    Move to main branch for treesitter-nvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,54 +1,39 @@
 return {
   -- https://github.com/nvim-treesitter/nvim-treesitter
-  { "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    keys = {
-      { "<c-space>", desc = "Increment selection" },
-      { "<bs>", desc = "Decrement selection", mode = "x" },
-    },
-    opts = {
-      highlight = {enable=true},
-      indent = {enable=true},
-      matchup = {enable=true},
-      ensure_installed = {
-        "bash", "bibtex", "c", "cpp", "css", "dockerfile", "fish",
-        "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",
-        "go", "gomod", "gosum", "html", "java", "javascript", "json",
-        "lua", "make", "markdown", "markdown_inline", "perl", "php", "python",
-        "rust", "sql", "toml", "tsx", "typescript", "vim", "yaml",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = false,
-          node_decremental = '<bs>',
-        },
-      },
-      textsubjects = {
-        enable = true,
-        keymaps = {
-          ["."] = "textsubjects-smart",
-          [";"] = "textsubjects-container-outer",
-        },
-      },
-    },
-    --@param opts TSConfig
-    config = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        ---@type table<string, boolean>
-        local added = {}
-        opts.ensure_installed = vim.tbl_filter(function(lang)
-          if added[lang] then
-            return false
-          end
-          added[lang] = true
-          return true
-        end, opts.ensure_installed)
-      end
-      require("nvim-treesitter.configs").setup(opts)
-    end
-  },
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  branch = "main",
+  build = ":TSUpdate",
+  config = function()
+    -- require'nvim-treesitter'.install({
+    --   "astro", "bash", "c", "cpp", "css", "dockerfile",
+    --   "fish", "git_rebase", "gitattributes", "gitcommit",
+    --   "go", "html", "java", "javascript", "json", "lua",
+    --   "latex", "make", "markdown", "markdown_inline",
+    --   "perl", "php", "python", "rust", "sql", "toml",
+    --   "tsx", "typescript", "yaml",
+    -- })
+
+    local group = vim.api.nvim_create_augroup('TreesitterSetup', { clear = true })
+
+    local ignore_filetypes = {
+      'checkhealth',
+      'lazy',
+      'mason',
+    }
+
+    -- highlighting
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { '<filetype>' },
+      callback = function() vim.treesitter.start() end,
+    })
+
+    -- folding
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
+
+    -- indentation
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+  end
 }
