68cc99efda63963e04b1a66de2ca5185e7e31f9f
ishrq <p.git@ishrq.com>
2025-12-18 14:49:28 +0600

    Edit comments

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,10 +1,8 @@
--- json pretty print
+-- pretty print with `jq`
 vim.keymap.set('n', '<C-p>', ':%!jq .<CR>')
 
--- on pressing 'o' to create a new line
--- append comma ',' at end of previous line
 -- https://github.com/JoosepAlviste/dotfiles/blob/master/config/nvim/ftplugin/json.lua
-
+-- pressing 'o' to create new line appends comma ',' at prev line
 vim.keymap.set('n', 'o', function()
   local line = vim.api.nvim_get_current_line()
 
